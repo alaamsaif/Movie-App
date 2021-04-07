9540612a0c4fd93f4af54fed2fd9cb4740c8a9d5
alaamsaif <alaamohammedsaif@gmail.com>
2021-04-07 06:05:25 +0200

    some updates

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,14 @@
 export const environment = {
-  production: true
+  production: true,
+  apiBase: 'https://api.themoviedb.org/3',
+  apiKey: '?api_key=1a5602c2cce1df66d9735323fe31b85d',
+  firebase : {
+    apiKey: "AIzaSyDL9bwG8BghlGZCbLcNaJS1YQ_4OP-LPUY",
+    authDomain: "movieapp-92c95.firebaseapp.com",
+    projectId: "movieapp-92c95",
+    storageBucket: "movieapp-92c95.appspot.com",
+    messagingSenderId: "683529852271",
+    appId: "1:683529852271:web:1c9933a3b339dce738449a"
+  }
+
 };
